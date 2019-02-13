cc0ae3d490e1cea2749591ade233bac164f880be
Mitchell Hayes <hayes_mitchell@yahoo.com>
2019-02-13 11:27:42 -0800

    still have formating i can do and it does not stop after reaching 21

--- reformat-blackjack-to-javascript/blackjack.js
+++ reformat-blackjack-to-javascript/blackjack.js
@@ -1,4 +1,4 @@
-var cards = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+var deck = {"A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
          "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10};
 
 
@@ -7,23 +7,46 @@ var total = [0];
 function getSum(total, num) {
   return total + num;
 }
-while (total.reduce(getSum) < 18){
-    if (total.reduce(getSum) == 0) {
-        total.push(cards[prompt("What is your first card?  >>  ")])
-        total.push(cards[prompt("What is you second card?  >>  ")])
+
+let cardCount = 0;
+
+let cards = document.getElementsByClassName('card');
+for(i=0; i<cards.length; i++){
+cards[i].addEventListener("click", function(e){
+e.preventDefault();
+total.push(deck[this.innerText]);
+let Total = document.createElement("p");
+Total.innerText = `${total.reduce(getSum)}`;
+cardCount +=1
+document.getElementById("total").removeChild(document.querySelector('p'))
+document.getElementById("total").appendChild(Total);
+    if (total.reduce(getSum) > 21 && total.includes(11)){
+        total.splice(total.indexOf(11), 1)
+        total.push(1)
+        }
+    if (cardCount < 2) {
+       document.getElementById("print").innerText = "you don't have 2 cards yet."
 }
     else if (total.reduce(getSum) < 17){
-        alert(total.reduce(getSum) + " Hit")
-        total.push(cards[prompt("whats your next card? >> ")])
+        document.getElementById("print").innerText = "Hit"
         }
-    if (17 < total.reduce(getSum) &&  total.reduce(getSum) < 21){
-        alert(total.reduce(getSum) +" is pretty good, you should stay. ")}
+    else if (17 < total.reduce(getSum) &&  total.reduce(getSum) < 21){
+        document.getElementById("print").innerText = "you should stay. "
+        }
     else if (total.reduce(getSum) == 21){
-        alert("winner winner, chicken dinner.")}
-    else if (total.reduce(getSum) > 21 && total.includes(11)){
-        total.splice(total.indexOf(11), 1)
-        total.push(1)}
-    else if (total.reduce(getSum) > 21){
-        alert(total.reduce(getSum) + " you went over 21, you lose")
+       document.getElementById("print").innerText = "winner winner, chicken dinner."
         }
+    else if (total.reduce(getSum) > 21){
+        document.getElementById("print").innerText = " you went over 21, you lose"
+};
+})
 }
+
+
+
+
+
+// e.preventDefault();
+//                    let final = document.createElement("li");
+//                    final.innerText = `${distance.value}  ${currentUnits.value} equals ${conversion} ${finalUnits.value}`;
+//                    document.getElementById("ul").appendChild(final);
